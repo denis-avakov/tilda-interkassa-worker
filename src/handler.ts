6b61141ae29dfbe0a9e2ffddf96cbfb9e3572321
Denis Avakov <denis.avakov@pm.me>
2022-04-08 15:01:48 +0300

    feat: create a worker

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,47 @@
+import { nanoid } from 'nanoid/non-secure';
+import * as sha256 from 'crypto-js/sha256';
+import * as Base64 from 'crypto-js/enc-base64';
+
+function createSignature(submitFormData: any) {
+  const keys = Object.keys(submitFormData).sort((a, b) => a.localeCompare(b));
+  const values = keys.map((key) => submitFormData[key]).concat(INTERKASSA_SECRET_KEY);
+
+  return values.join(':');
+}
+
+async function generatePaymentId(): Promise<number | string> {
+  try {
+    const paymentNumber = await TILDA_INTERKASSA.get('payment-number');
+    await TILDA_INTERKASSA.put('payment-number', (Number(paymentNumber) + 1).toString());
+
+    return Number(paymentNumber) + 1;
+  } catch (error) {
+    console.log('Error in generatePaymentId()', error);
+  }
+
+  return nanoid(10);
+}
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`Request method: ${request.method}`);
+  const paymentId = await generatePaymentId();
+  const paymentProps = await request.json();
+
+  const submitFormData = Object.assign(paymentProps, {
+    ik_co_id: INTERKASSA_CHECKOUT_ID,
+    ik_pm_no: `ID_${paymentId}`
+  });
+
+  const signature = createSignature(submitFormData);
+  const signatureHash = Base64.stringify(sha256(signature));
+
+  const responseJSON = JSON.stringify({
+    ik_pm_no: `ID_${paymentId}`,
+    ik_sign: signatureHash
+  });
+
+  return new Response(responseJSON, {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8'
+    }
+  });
 }
